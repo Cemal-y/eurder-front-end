3e00238d49b85963d165ffd666fb200a93753ce0
Cemal-y <yildiz.cml@gmail.com>
2020-05-07 16:34:19 +0200

    added configuration for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static('./dist/eurder-front-end'));
 
 app.listen(process.env.PORT || 8080);
